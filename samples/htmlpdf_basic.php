e3fd029263b16fc76e6bf8d7551c5df35f632331
Fiaz <fiaz98975@gmail.com>
2024-07-22 16:02:02 +0500

    v1.1

--- samples/htmlpdf_basic.php
+++ samples/htmlpdf_basic.php
@@ -2,10 +2,10 @@
 //include the autoloader
 require_once('../vendor/autoload.php');
 //if manual installation has been used comment line that requires the autoload and uncomment this line:
-//require_once('../init.php');
+//require_once('../init.php')
 
 use Ilovepdf\HtmlpdfTask;
-
+// its the verison 1
 
 // you can call task class directly
 // to get your key pair, please visit https://developer.ilovepdf.com/user/projects
